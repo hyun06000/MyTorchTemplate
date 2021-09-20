b7b56801e8433b6b4078c30189b204f685c2a5c0
Sang-hyun Park <hyun06000@gmail.com>
2021-09-21 02:19:53 +0900

    20210921 config_generator

--- sweep_config/sweep_config_grid.py
+++ sweep_config/sweep_config_grid.py
@@ -5,10 +5,10 @@ sweep_config_grid = {
             "value" : 5
         },
         "BATCH_SIZE" : {
-            "values" : [16, 32, 64, 128]
+            "values" : [32, 64, 128]
         },
         "LEARNING_RATE" :{
-            "min": 0.0001, "max": 0.1
+            "values" : [0.001, 0.01, 0.1]
         },
         "SAVEFIG_NAME"  : {
             "value":"result.png"
